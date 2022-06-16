a689a9e94560abdaf2a48561cfc96cf14f080c3e
The Oddball <oddball@oddball.tf>
2022-06-15 19:09:19 -0700

    Update CSGOStats-Multisearch-Open.user.js

--- CSGOStats-Multisearch-Open.user.js
+++ CSGOStats-Multisearch-Open.user.js
@@ -11,6 +11,7 @@
 // @updateURL      https://raw.githubusercontent.com/theoddball/CSGOStats-Multisearch-Open/master/CSGOStats-Multisearch-Open.user.js
 // @downloadURL    https://raw.githubusercontent.com/theoddball/CSGOStats-Multisearch-Open/master/CSGOStats-Multisearch-Open.user.js
 // @supportURL     https://github.com/theoddball/CSGOStats-Multisearch-Open/issues
+// @require        https://code.jquery.com/jquery-3.6.0.slim.min.js
 // ==/UserScript==
 var buttonLocation = $("#multi-lookup");
 var button = `<button id="button" type="button">Open all stats pages</button>`
