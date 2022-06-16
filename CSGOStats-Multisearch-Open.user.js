9af81e932118417727bc3f18cc7988af547004e8
The Oddball <oddball@oddball.tf>
2022-06-15 19:07:07 -0700

    Create CSGOStats-Multisearch-Open.user.js

---
+++ CSGOStats-Multisearch-Open.user.js
@@ -0,0 +1,28 @@
+// ==UserScript==
+// @name           CSGOStats-Multisearch-Open
+// @namespace      http://github.com/theoddball
+// @author         theoddball
+// @version        1.0
+// @description    Opens multiple stats pages at once
+// @include        http://csgostats.gg/player/multi*
+// @include        https://csgostats.gg/player/multi*
+// @grant          none
+// @homepageURL    https://github.com/theoddball/CSGOStats-Multisearch-Open
+// @updateURL      https://raw.githubusercontent.com/theoddball/CSGOStats-Multisearch-Open/master/CSGOStats-Multisearch-Open.user.js
+// @downloadURL    https://raw.githubusercontent.com/theoddball/CSGOStats-Multisearch-Open/master/CSGOStats-Multisearch-Open.user.js
+// @supportURL     https://github.com/theoddball/CSGOStats-Multisearch-Open/issues
+// ==/UserScript==
+var buttonLocation = $("#multi-lookup");
+var button = `<button id="button" type="button">Open all stats pages</button>`
+buttonLocation.after(button);
+$("#button").click(run);
+function run() {
+    var x = document.querySelectorAll("a");
+    for (var i = 0; i < x.length; i++) {
+        var cleanlink = x[i].href;
+        if (/https:\/\/steamcommunity\.com\/profiles\/.*/g.test(cleanlink)) {
+            var openlink = cleanlink.replace("steamcommunity.com/profiles", "csgostats.gg/player");
+            window.open(openlink, '_blank');
+        }
+    }
+}
